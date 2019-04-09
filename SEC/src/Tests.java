bcde50120059c31370d22267bd2887e1fa3f720b
andremendes95 <andre.filipe.m.mendes@gmail.com>
2019-04-09 15:19:34 +0100

    changes

--- SEC/src/Tests.java
+++ SEC/src/Tests.java
@@ -1,6 +1,5 @@
 import static org.junit.Assert.assertTrue;
 import static org.junit.jupiter.api.Assertions.*;
-
 import java.io.FileInputStream;
 import java.io.IOException;
 import java.net.Socket;
@@ -13,34 +12,43 @@ import java.security.NoSuchAlgorithmException;
 import java.security.PrivateKey;
 import java.security.PublicKey;
 import java.util.HashMap;
-
 import javax.security.cert.X509Certificate;
-
 import org.junit.jupiter.api.Test;
-
-
 
 public class Tests {
 
 	Client client = new Client("localhost",1500,"test");
+	
 	Notary server = new Notary(1500);
+	
 	RSA rsa = new RSA();
 	
 	@Test
 	public void testPrivateKeys() throws NoSuchAlgorithmException, GeneralSecurityException, IOException{
+		
 		KeyPair kPair = rsa.createKeyPairs("testKey");
+		
 		PrivateKey expectedPrivKey = rsa.checkPrivateKey(kPair);
+		
 		PublicKey pubKey = rsa.checkPublicKey("testKey", kPair);
+		
 		rsa.createCert("testKey", pubKey, expectedPrivKey);
+		
 		PrivateKey actualPrivKey = client.getPrivateKey("testKey");
+		
 		assertEquals(expectedPrivKey,actualPrivKey);
 		
 	}
+	
 	@Test
 	public void testPublicKeys() throws NoSuchAlgorithmException, GeneralSecurityException, IOException{
+		
 		KeyPair kPair = rsa.createKeyPairs("testkey");
+		
 		PublicKey expectedPubKey = rsa.checkPublicKey("testkey", kPair);
+		
 		PublicKey actualPubKey = client.readPublicKeyFromFile("testkey" + "public.key");
+		
 		assertEquals(expectedPubKey,actualPubKey);
 		
 	}
